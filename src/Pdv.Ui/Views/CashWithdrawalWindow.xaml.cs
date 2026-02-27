ed40087335aae978439521895bc70e0cb7b986ec
Arthur Bessa <99930110+arthursbessa@users.noreply.github.com>
2026-02-27 00:23:03 -0300

    Redesenha menu operacional e separa fluxo de caixas

---
+++ src/Pdv.Ui/Views/CashWithdrawalWindow.xaml.cs
@@ -0,0 +1,24 @@
+using System.Windows;
+using Pdv.Ui.ViewModels;
+
+namespace Pdv.Ui.Views;
+
+public partial class CashWithdrawalWindow : Window
+{
+    public CashWithdrawalWindow()
+    {
+        InitializeComponent();
+    }
+
+    private async void Confirm_Click(object sender, RoutedEventArgs e)
+    {
+        if (DataContext is not MenuViewModel vm)
+        {
+            return;
+        }
+
+        await vm.RegisterWithdrawalAsync(AmountTextBox.Text, ReasonTextBox.Text);
+        DialogResult = true;
+        Close();
+    }
+}
